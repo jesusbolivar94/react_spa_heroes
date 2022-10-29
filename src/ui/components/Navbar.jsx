c0c188f82cb0f5740054df720b0da68b04134652
Jesus Bolivar <j5rojo94@gmail.com>
2022-10-28 23:32:55 -0300

    Crea navegaciòn entre login y logout

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,17 @@
-import {Link, NavLink} from 'react-router-dom'
+import {Link, NavLink, useNavigate} from 'react-router-dom'
 
 export const Navbar = () => {
+
+    const navigate = useNavigate()
+
+    const onLogOut = () => {
+
+        navigate('/login', {
+            replace: true
+        })
+
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
 
@@ -35,7 +46,7 @@ export const Navbar = () => {
 
                     <span className="nav-item nav-link text-info">Jesus</span>
 
-                    <button className="nav-item nav-link btn">
+                    <button className="nav-item nav-link btn" onClick={ onLogOut }>
                         Logout
                     </button>
 
