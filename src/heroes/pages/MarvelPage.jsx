565209ba7f00463f108e11db2147eb7937be6c63
Jesus Bolivar <j5rojo94@gmail.com>
2022-10-28 23:43:10 -0300

    Muestra listado de heroes de DC y Marvel

--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,9 +1,13 @@
+import {HeroList} from '../components/index.js'
 
 
 export const MarvelPage = () => {
     return (
         <>
             <h1>Marvel Page</h1>
+            <hr/>
+
+            <HeroList publisher='Marvel Comics'  />
         </>
     )
 }
